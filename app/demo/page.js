799a4c99651856201458f2daed2c25163dc359e6
urielurbina <soyurielurbina@gmail.com>
2024-10-15 16:12:39 -0600

    menu con logo

--- app/demo/page.js
+++ app/demo/page.js
@@ -56,7 +56,7 @@ export default function Menu() {
           {/* Header with background image and logo */}
           <div className="relative">
             <Image
-              src="/placeholder.svg?height=200&width=200"
+              src="/images/trompo-de-pastor.webp"
               alt="Fondo de tacos"
               width={500}
               height={300}
@@ -64,34 +64,45 @@ export default function Menu() {
               objectFit="cover"
             />
             <div className="absolute inset-0 flex items-center justify-center">
-              <div className="bg-black rounded-full w-24 h-24 flex items-center justify-center">
-                <span className="text-white text-lg font-bold">Logotipo</span>
+              <div className="bg-white rounded-full w-32 h-32 flex items-center justify-center overflow-hidden">
+                <Image
+                  src="/images/logo-tacos-uriel.png"
+                  alt="Logotipo"
+                  width={128}
+                  height={128}
+                  objectFit="cover"
+                />
               </div>
             </div>
           </div>
 
-          {/* Business information */}
-          <div className="px-6 py-4">
-            <h1 className="text-2xl font-bold tracking-tight">Tacos el Uriel</h1>
-            <p className="text-sm mt-1">El mejor sabor en cada taco</p>
+          {/* Business information and Social links */}
+          <div className="px-6 py-4 flex">
+            {/* Business information */}
+            <div className="w-2/3 pr-4">
+              <h1 className="text-2xl font-bold tracking-tight">Tacos el Uriel</h1>
+              <p className="text-sm mt-1">El mejor sabor en cada taco</p>
+            </div>
 
             {/* Social links */}
-            <div className="flex justify-end space-x-2 mt-2">
-              <Link href="#" className="text-black">
-                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
-                </svg>
-              </Link>
-              <Link href="#" className="text-black">
-                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M12 0C8.74 0 8.333.015 7.053.072 5.775.132 4.905.333 4.14.63c-.789.306-1.459.717-2.126 1.384S.935 3.35.63 4.14C.333 4.905.131 5.775.072 7.053.012 8.333 0 8.74 0 12s.015 3.667.072 4.947c.06 1.277.261 2.148.558 2.913.306.788.717 1.459 1.384 2.126.667.666 1.336 1.079 2.126 1.384.766.296 1.636.499 2.913.558C8.333 23.988 8.74 24 12 24s3.667-.015 4.947-.072c1.277-.06 2.148-.262 2.913-.558.788-.306 1.459-.718 2.126-1.384.666-.667 1.079-1.335 1.384-2.126.296-.765.499-1.636.558-2.913.06-1.28.072-1.687.072-4.947s-.015-3.667-.072-4.947c-.06-1.277-.262-2.149-.558-2.913-.306-.789-.718-1.459-1.384-2.126C21.319 1.347 20.651.935 19.86.63c-.765-.297-1.636-.499-2.913-.558C15.667.012 15.26 0 12 0zm0 2.16c3.203 0 3.585.016 4.85.071 1.17.055 1.805.249 2.227.415.562.217.96.477 1.382.896.419.42.679.819.896 1.381.164.422.36 1.057.413 2.227.057 1.266.07 1.646.07 4.85s-.015 3.585-.074 4.85c-.061 1.17-.256 1.805-.421 2.227-.224.562-.479.96-.899 1.382-.419.419-.824.679-1.38.896-.42.164-1.065.36-2.235.413-1.274.057-1.649.07-4.859.07-3.211 0-3.586-.015-4.859-.074-1.171-.061-1.816-.256-2.236-.421-.569-.224-.96-.479-1.379-.899-.421-.419-.69-.824-.9-1.38-.165-.42-.359-1.065-.42-2.235-.045-1.26-.061-1.649-.061-4.844 0-3.196.016-3.586.061-4.861.061-1.17.255-1.814.42-2.234.21-.57.479-.96.9-1.381.419-.419.81-.689 1.379-.898.42-.166 1.051-.361 2.221-.421 1.275-.045 1.65-.06 4.859-.06l.045.03zm0 3.678c-3.405 0-6.162 2.76-6.162 6.162 0 3.405 2.76 6.162 6.162 6.162 3.405 0 6.162-2.76 6.162-6.162 0-3.405-2.76-6.162-6.162-6.162zM12 16c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm7.846-10.405c0 .795-.646 1.44-1.44 1.44-.795 0-1.44-.646-1.44-1.44 0-.794.646-1.439 1.44-1.439.793-.001 1.44.645 1.44 1.439z"/>
-                </svg>
-              </Link>
-              <Link href="#" className="text-black">
-                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M18.48 22.926l-1.193.658c-6.979 3.621-19.082-17.494-12.279-21.484l1.145-.637 3.714 6.467-1.139.632c-2.067 1.245 2.76 9.707 4.879 8.545l1.162-.642 3.711 6.461zm-9.808-22.926l-1.68.975 3.714 6.466 1.681-.975-3.715-6.466zm8.613 14.997l-1.68.975 3.714 6.467 1.681-.975-3.715-6.467z"/>
-                </svg>
-              </Link>
+            <div className="w-1/3 flex justify-end items-center">
+              <div className="flex space-x-2">
+                <Link href="#" className="text-black">
+                  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
+                  </svg>
+                </Link>
+                <Link href="#" className="text-black">
+                  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12 0C8.74 0 8.333.015 7.053.072 5.775.132 4.905.333 4.14.63c-.789.306-1.459.717-2.126 1.384S.935 3.35.63 4.14C.333 4.905.131 5.775.072 7.053.012 8.333 0 8.74 0 12s.015 3.667.072 4.947c.06 1.277.261 2.148.558 2.913.306.788.717 1.459 1.384 2.126.667.666 1.336 1.079 2.126 1.384.766.296 1.636.499 2.913.558C8.333 23.988 8.74 24 12 24s3.667-.015 4.947-.072c1.277-.06 2.148-.262 2.913-.558.788-.306 1.459-.718 2.126-1.384.666-.667 1.079-1.335 1.384-2.126.296-.765.499-1.636.558-2.913.06-1.28.072-1.687.072-4.947s-.015-3.667-.072-4.947c-.06-1.277-.262-2.149-.558-2.913-.306-.789-.718-1.459-1.384-2.126C21.319 1.347 20.651.935 19.86.63c-.765-.297-1.636-.499-2.913-.558C15.667.012 15.26 0 12 0zm0 2.16c3.203 0 3.585.016 4.85.071 1.17.055 1.805.249 2.227.415.562.217.96.477 1.382.896.419.42.679.819.896 1.381.164.422.36 1.057.413 2.227.057 1.266.07 1.646.07 4.85s-.015 3.585-.074 4.85c-.061 1.17-.256 1.805-.421 2.227-.224.562-.479.96-.899 1.382-.419.419-.824.679-1.38.896-.42.164-1.065.36-2.235.413-1.274.057-1.649.07-4.859.07-3.211 0-3.586-.015-4.859-.074-1.171-.061-1.816-.256-2.236-.421-.569-.224-.96-.479-1.379-.899-.421-.419-.69-.824-.9-1.38-.165-.42-.359-1.065-.42-2.235-.045-1.26-.061-1.649-.061-4.844 0-3.196.016-3.586.061-4.861.061-1.17.255-1.814.42-2.234.21-.57.479-.96.9-1.381.419-.419.81-.689 1.379-.898.42-.166 1.051-.361 2.221-.421 1.275-.045 1.65-.06 4.859-.06l.045.03zm0 3.678c-3.405 0-6.162 2.76-6.162 6.162 0 3.405 2.76 6.162 6.162 6.162 3.405 0 6.162-2.76 6.162-6.162 0-3.405-2.76-6.162-6.162-6.162zM12 16c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm7.846-10.405c0 .795-.646 1.44-1.44 1.44-.795 0-1.44-.646-1.44-1.44 0-.794.646-1.439 1.44-1.439.793-.001 1.44.645 1.44 1.439z"/>
+                  </svg>
+                </Link>
+                <Link href="#" className="text-black">
+                  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M18.48 22.926l-1.193.658c-6.979 3.621-19.082-17.494-12.279-21.484l1.145-.637 3.714 6.467-1.139.632c-2.067 1.245 2.76 9.707 4.879 8.545l1.162-.642 3.711 6.461zm-9.808-22.926l-1.68.975 3.714 6.466 1.681-.975-3.715-6.466zm8.613 14.997l-1.68.975 3.714 6.467 1.681-.975-3.715-6.467z"/>
+                  </svg>
+                </Link>
+              </div>
             </div>
           </div>
 
@@ -101,11 +112,21 @@ export default function Menu() {
               <p className="font-bold">Dirección:</p>
               <p>Calle Principal 123,</p>
               <p>Ciudad Bonita</p>
+              <p>México</p>
             </div>
             <div className="text-right">
               <p className="font-bold">Horario:</p>
               <p>Lun-Vie 11:00-22:00</p>
-              <p>Sáb-Dom 12:00-23:00</p>
+              <p>Sáb 12:00-23:00</p>
+              <p>Dom Cerrado</p>
+            </div>
+          </div>
+
+          {/* Business description */}
+          <div className="px-6 py-3 flex justify-between text-xs">
+            <div className="text-left">
+              
+              <p>Lorem ipsum dolor sit amet consectetur. Risus augue egestas augue orci congue ipsum. Interdum proin luctus purus tincidunt dolor nulla nisi. Ullamcorper fermentum elit lorem montes massa commodo vitae amet. Vel ultrices imperdiet consectetur ac dignissim tempor in augue.</p>
             </div>
           </div>
 
@@ -129,7 +150,7 @@ export default function Menu() {
 
         {/* Columns 2-4: Categories and Products */}
         <div className="lg:w-3/4 lg:ml-[25%]">
-          <div className="bg-white">
+          <div className="bg-gray-100">
             {/* Categories */}
             <div 
               className={`px-6 py-4 ${isScrolled ? 'lg:fixed lg:top-0 lg:right-0 lg:left-[25%] lg:bg-white lg:z-20' : ''}`}
@@ -157,15 +178,15 @@ export default function Menu() {
 
             {/* Products */}
             <div className={`px-6 py-4 ${isScrolled ? 'lg:mt-[100px]' : ''}`}>
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {products.map((product) => (
                   <div 
                     key={product.id}
-                    className="bg-white rounded-lg overflow-hidden shadow-md cursor-pointer"
+                    className="bg-white rounded-lg overflow-hidden cursor-pointer"
                     onClick={() => toggleProductDetails(product)}
                   >
                     <div className="p-3">
-                      <div className="aspect-square bg-gray-200" />
+                      <div className="aspect-square bg-gray-200 rounded-sm" />
                       <span className="text-xs bg-gray-200 px-2 py-1 rounded-full mt-2 inline-block">
                         {product.category}
                       </span>
