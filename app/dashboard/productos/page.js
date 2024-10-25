e4b5ed5fe1caf86db34d2861256e2734b270a8ad
urielurbina <soyurielurbina@gmail.com>
2024-10-24 23:04:01 -0600

    modal producto test 9

--- app/dashboard/productos/page.js
+++ app/dashboard/productos/page.js
@@ -415,9 +415,9 @@ export default function ProductDashboard() {
 
       {/* Modal para agregar/editar producto */}
       {isAddingProduct && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-start justify-center p-4 overflow-y-auto ">
-          <div className="bg-white rounded-lg w-full max-w-2xl max-h-[90vh] flex flex-col shadow-lg m-2 sm:m-4 mt-8 z-2">
-            <h2 className="text-xl sm:text-2xl font-bold mb-2 sm:mb-4 text-gray-900 sticky top-0 bg-white py-3 px-4 sm:px-6 border-b z-10">
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-start justify-center p-4 overflow-y-auto z-2">
+          <div className="bg-white rounded-lg w-full max-w-2xl max-h-[90vh] flex flex-col shadow-lg m-2 sm:m-4 mt-8">
+            <h2 className="text-xl sm:text-2xl font-bold mb-2 sm:mb-4 text-gray-900 sticky top-0 bg-white py-3 px-4 sm:px-6 border-b">
               {editingProduct ? "Editar" : "Agregar"} Producto
             </h2>
             <div className="flex-grow overflow-y-auto px-4 sm:px-6 py-4">
