f575464f25ace64c2754d49688c9e0b858811039
urielurbina <soyurielurbina@gmail.com>
2024-10-24 23:36:46 -0600

    modal producto archivo

--- app/dashboard/productos/page.js
+++ app/dashboard/productos/page.js
@@ -452,23 +452,20 @@ export default function ProductDashboard() {
                       {newProduct.imagen ? (
                         <div>
                           <Image src={newProduct.imagen} alt="Preview" width={200} height={200} className="mx-auto object-cover rounded-md" />
-                          <p className="text-sm text-gray-500 mt-2">Haz clic o arrastra una nueva imagen para cambiarla</p>
                         </div>
                       ) : (
-                        <>
-                          <svg className="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                            <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
-                          </svg>
-                          <div className="flex text-sm text-gray-600 justify-center">
-                            <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-[#0D654A] hover:text-[#0D654A] focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-[#0D654A]">
-                              <span>Sube un archivo</span>
-                              <input {...getInputProps()} id="file-upload" name="file-upload" type="file" className="sr-only" />
-                            </label>
-                            <p className="pl-1">o arrastra y suelta</p>
-                          </div>
-                          <p className="text-xs text-gray-500">PNG, JPG hasta 10MB</p>
-                        </>
+                        <svg className="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                          <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
+                        </svg>
                       )}
+                      <div className="flex text-sm text-gray-600 justify-center">
+                        <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-[#0D654A] hover:text-[#0D654A] focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-[#0D654A]">
+                          <span>Sube un archivo</span>
+                          <input {...getInputProps()} id="file-upload" name="file-upload" type="file" className="sr-only" />
+                        </label>
+                        <p className="pl-1">o arrastra y suelta</p>
+                      </div>
+                      <p className="text-xs text-gray-500">PNG, JPG hasta 10MB</p>
                     </div>
                   </div>
                 </div>
