9f657acc66736918f5580548092553856770abc8
urielurbina <soyurielurbina@gmail.com>
2024-10-24 23:14:53 -0600

    modal producto test 10

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -127,7 +127,7 @@ export default function ResponsiveLayout({ children }) {
 
         <div className="flex flex-col w-0 flex-1 overflow-hidden">
           {/* Botón de menú para móvil */}
-          <div className="md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3 z-0">
+          <div className="md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3">
             <button
               className="h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-[#0D654A]"
               onClick={() => setIsMobileMenuOpen(true)}
@@ -138,7 +138,7 @@ export default function ResponsiveLayout({ children }) {
               </svg>
             </button>
           </div>
-          <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none">
+          <main className="flex-1 relative overflow-y-auto focus:outline-none">
             <div className="py-6 md:py-6">
               <div className="max-w-full mx-auto px-2 sm:px-4 md:px-8 flex flex-col xl:flex-row">
                 {/* Contenido del dashboard */}
