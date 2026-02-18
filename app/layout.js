1b739d866b6937f7e24588aa67b36735a16c2afe
urielurbina <soyurielurbina@gmail.com>
2026-02-18 12:27:15 -0600

    Add Microsoft Clarity script for enhanced user behavior tracking and insights in layout.js.

--- app/layout.js
+++ app/layout.js
@@ -52,6 +52,18 @@ export default function RootLayout({ children }) {
             data-website-id="0c78c6a2-9b88-4d62-8337-d7e6f4bfb386"
             strategy="afterInteractive"
           />
+          <Script
+            id="microsoft-clarity"
+            strategy="afterInteractive"
+          >
+            {`
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "vjfh1hviur");
+            `}
+          </Script>
         </head>
       )}
       <body>
